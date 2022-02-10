d541b15d97ab4e11a24eeaece280825015d0f2e9
project-esb <85157731+project-esb@users.noreply.github.com>
2022-02-10 18:24:57 +0530

    Update flames_app.py

--- flames_app.py
+++ flames_app.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.markdown('<center><h1>🅵🅻🅰🅼🅴🆂</h1></center>',unsafe_allow_html=True)
 
-page_bg_img = '''
+page_bg_img = """
 <style>
 body {
 background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
 background-size: cover;
 }
 </style>
-'''
+"""
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
