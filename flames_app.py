788f4da76268082e70c4ab99f9faf01f555c12ad
project-esb <85157731+project-esb@users.noreply.github.com>
2022-02-10 18:34:58 +0530

    Update flames_app.py

--- flames_app.py
+++ flames_app.py
@@ -12,6 +12,18 @@ background-size: cover;
 st.markdown(page_bg_img, unsafe_allow_html=True)
 st.markdown('<center><h1>🅵🅻🅰🅼🅴🆂</h1></center>',unsafe_allow_html=True)
 
+st.set_page_config(
+     page_title="Ex-stream-ly Cool App",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
+
 
 
 
