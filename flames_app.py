83c11b30052d57f3a916a19595a41bf3182acd22
project-esb <85157731+project-esb@users.noreply.github.com>
2022-02-10 17:25:53 +0530

    Update flames_app.py

--- flames_app.py
+++ flames_app.py
@@ -1,14 +1,5 @@
 import streamlit as st
 
 st.markdown(
-<!DOCTYPE html>
-<html>
-  
-<body>
-    <h2>Welcome to GeeksforGeeks</h2>
-    <!--center tag is used here-->
     <center>Hi Geeks</center>
-  
-</body>
-  
-</html>, unsafe_allow_html=False)
+, unsafe_allow_html=False)
