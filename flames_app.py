54b9e5763aca5f7b9dcedcb9626b4ed945e3c994
project-esb <85157731+project-esb@users.noreply.github.com>
2022-02-10 18:15:11 +0530

    Update flames_app.py

--- flames_app.py
+++ flames_app.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.markdown('<center><h1>🅵🅻🅰🅼🅴🆂</h1></center>',unsafe_allow_html=True)
+
+ st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://pixabay.com/images/id-3061483/")
+    }
+   .sidebar .sidebar-content {
+        background: url("https://pixabay.com/images/id-3061483/")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
