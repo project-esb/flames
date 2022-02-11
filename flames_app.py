d9dcbdb3964deea65a1dd24516bcbdad4f3d0c19
project-esb <85157731+project-esb@users.noreply.github.com>
2022-02-11 11:10:18 +0530

    Update flames_app.py

--- flames_app.py
+++ flames_app.py
@@ -1,26 +1,7 @@
 import streamlit as st
 
-def set_bg_hack_url():
-    '''
-    A function to unpack an image from url and set as bg.
-    Returns
-    -------
-    The background.
-    '''
-        
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background: url("https://cdn.pixabay.com/photo/2020/06/19/22/33/wormhole-5319067_960_720.jpg");
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
 st.markdown('<center><h1>🅵🅻🅰🅼🅴🆂</h1></center>',unsafe_allow_html=True)
-set_bg_hack_url()
+
 
 
 
