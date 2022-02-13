210190c43d252e339ffffe4cb8263042386ab70a
project-esb <85157731+project-esb@users.noreply.github.com>
2022-02-13 18:49:24 +0530

    Update flames_app.py

--- flames_app.py
+++ flames_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
 st.markdown('<center><h1>🅵🅻🅰🅼🅴🆂</h1></center>',unsafe_allow_html=True)
+st.markdown('<center><h1>Enter Your Name</h1></center>',unsafe_allow_html=True)
+st.text_input()
 
 
 
