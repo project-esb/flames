950e927da62e9ed18c615b1c39b717efdd267717
project-esb <85157731+project-esb@users.noreply.github.com>
2022-02-14 00:44:07 +0530

    Update flames_app.py

--- flames_app.py
+++ flames_app.py
@@ -13,7 +13,7 @@ def set_bg_hack_url():
          f"""
          <style>
          .stApp {{
-             background: url("https://cdn.pixabay.com/photo/2020/06/19/22/33/wormhole-5319067_960_720.jpg");
+             background: url("https://images.app.goo.gl/GyxH6QDveSJX3XcRA");
              background-size: cover
          }}
          </style>
